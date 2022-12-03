e5dcfff6478e2274f8310d894d2e237dce857751
chrisfarms <chris@farmiloe.com>
2022-12-03 17:02:40 +0000

    rough port of the SessionRouter to cog

---
+++ src/SessionRouter.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { State } from "./State.sol";
+import { Context, Router, Dispatcher } from "./Dispatcher.sol";
+
+import { LibString } from "solmate/utils/LibString.sol";
+using { LibString.toString } for uint256;
+
+bytes constant PREFIX_MESSAGE = "\x19Ethereum Signed Message:\n";
+bytes constant AUTHEN_MESSAGE = "You are signing in with session: ";
+bytes constant REVOKE_MESSAGE = "You are signing out of session: ";
+
+error SessionExpiryTooLong();
+error SessionUnauthorized();
+error SessionExpired();
+
+contract SessionRouter is Router {
+
+    uint constant MAX_TTL = 40000;
+
+    struct Session {
+        Dispatcher dispatcher;
+        address owner;
+        uint32 exp;
+        uint32 scopes;
+    }
+
+    mapping(address => Session) public sessions;
+
+    // authorizeKey delegates permissions to key to act as msg.sender when talking to dispatcher
+    function authorizeAddr(
+        Dispatcher dispatcher,
+        uint32 ttl,
+        uint32 scopes,
+        address addr
+    ) public {
+        sessions[addr] = Session({
+            dispatcher: dispatcher,
+            owner: msg.sender,
+            exp: expires(ttl),
+            scopes: scopes
+        });
+    }
+
+    // authorizeKey delegates permissions to key to act as the signer of v/r/s when talking to dispatcher
+    function authorizeAddr(
+        Dispatcher dispatcher,
+        uint32 ttl,
+        uint32 scopes,
+        address addr,
+        uint8 v, bytes32 r, bytes32 s
+    ) public {
+        address owner = ecrecover(keccak256(abi.encodePacked(
+            PREFIX_MESSAGE,
+            (AUTHEN_MESSAGE.length+20).toString(),
+            AUTHEN_MESSAGE,
+            addr
+        )) , v, r, s);
+        sessions[addr] = Session({
+            dispatcher: dispatcher,
+            owner: owner,
+            exp: expires(ttl),
+            scopes: scopes
+        });
+    }
+
+    // revokeKey expires the session key, requires msg.sender to be owner of the session
+    function revokeAddr(
+        address addr
+    ) public {
+        Session storage session = sessions[addr];
+        if (session.owner != msg.sender) {
+            revert SessionUnauthorized();
+        }
+        delete sessions[addr];
+    }
+
+    // revokeKey expires the session key, requires signer of v/r/s to be session owner
+    function revokeAddr(
+        address addr,
+        uint8 v, bytes32 r, bytes32 s
+    ) public {
+        address owner = ecrecover(keccak256(abi.encodePacked(
+            PREFIX_MESSAGE,
+            (REVOKE_MESSAGE.length+20).toString(),
+            REVOKE_MESSAGE,
+            addr
+        )) , v, r, s);
+        Session storage session = sessions[addr];
+        if (session.owner != owner) {
+            revert SessionUnauthorized();
+        }
+        delete sessions[addr];
+    }
+
+    // dispatch expects actionSig to be either:
+    // - a valid sig (v/r/s) of the action data, in which case we treat the SIGNER as the session key
+    // - an empty sig, in which case we treat the SENDER as the session key
+    // session
+    // if the key has not expired, the target dispatcher is called with the generated context
+    //
+    // +-----------------------------------------------------------------------------------------+
+    // | [!] CRITICAL TODO: there is currently no replay protection for session signed actions! |
+    // +-----------------------------------------------------------------------------------------+
+    //
+    function dispatch(
+        bytes calldata action,
+        uint8 v, bytes32 r, bytes32 s
+    ) public {
+        Session storage session;
+        if (r == 0 && s == s && v == 0) {
+            // no signature provided, so we treat the sender as the session key
+            // this is useful for authorizing external contract addresses to act
+            // on behalf of the player
+            session = sessions[msg.sender];
+        } else {
+            // ecrecover sender from sig as key to lookup session info
+            // this is the path for when a player is using a temporary
+            // short lived session key in their client to sign actions
+            address signer = ecrecover(keccak256(abi.encodePacked(
+                "\x19Ethereum Signed Message:\n32",
+                keccak256(action)
+            )) , v, r, s);
+            session = sessions[signer];
+        }
+        if (session.owner == address(0)) {
+            revert SessionUnauthorized();
+        }
+        if (block.number > session.exp) {
+            revert SessionExpired();
+        }
+        // TODO: replay protection
+        Context memory ctx = Context({
+            sender: session.owner,
+            scopes: session.scopes
+        });
+        // forward to the dispatcher registered with the session
+        session.dispatcher.dispatch(action, ctx);
+    }
+
+    // expires converts a ttl to a future block number
+    // reverts if requested ttl "too long"
+    function expires(uint32 ttl) internal view returns (uint32) {
+        if (ttl > MAX_TTL) { // TODO: make this configurable
+            revert SessionExpiryTooLong();
+        }
+        return uint32(block.number + ttl);
+    }
+}
---
+++ test/SessionRouter.t.sol
@@ -0,0 +1,167 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {
+    ActionTypeDef,
+    BaseDispatcher,
+    Dispatcher,
+    DispatchUntrustedSender,
+    Rule,
+    Context
+} from "../src/Dispatcher.sol";
+import {State, NodeTypeUtils, NodeType, NodeData} from "../src/State.sol";
+import {StateGraph} from "../src/StateGraph.sol";
+import {
+    SessionRouter,
+    SessionUnauthorized,
+    PREFIX_MESSAGE,
+    AUTHEN_MESSAGE,
+    REVOKE_MESSAGE
+} from "../src/SessionRouter.sol";
+
+import "./utils/TestActions.sol";
+import "./utils/TestRules.sol";
+import "./utils/TestStateUtils.sol";
+using StateTestUtils for State;
+
+import { LibString } from "solmate/utils/LibString.sol";
+using { LibString.toString } for uint256;
+
+contract ExampleDispatcher is Dispatcher, BaseDispatcher {
+    constructor(State s) BaseDispatcher(s) {
+        registerRule(new LogSenderRule());
+    }
+}
+
+contract SessionRouterTest is Test {
+
+    State state;
+    BaseDispatcher dispatcher;
+    SessionRouter router;
+
+    uint256 ownerKey = 0xA11CE;
+    address ownerAddr = vm.addr(ownerKey);
+
+    uint256 sessionKey = 0x5e55;
+    address sessionAddr = vm.addr(sessionKey);
+
+    uint256 relayKey = 0x11111;
+    address relayAddr = vm.addr(relayKey);
+
+    function setUp() public {
+        state = new StateGraph(); // TODO: replace with a mock
+        dispatcher = new ExampleDispatcher(state);
+        router = new SessionRouter();
+        dispatcher.registerRouter(address(router));
+    }
+
+    function testSanityCheckAddrs() public {
+        assertFalse(ownerAddr == address(0));
+        assertFalse(sessionAddr == address(0));
+        assertFalse(sessionAddr == ownerAddr);
+        assertFalse(relayAddr == address(0));
+        assertFalse(relayAddr == ownerAddr);
+    }
+
+    function testAuthorizeAddrWithSenderAsOwner() public {
+        // authorize a session key by talking direct to
+        // contract as sender
+        vm.prank(ownerAddr);
+        router.authorizeAddr(dispatcher, 0, 0, sessionAddr);
+
+        // should now be able to use sessionKey to submit signed actions
+        // that act as the original owner
+        dispatchSigned(sessionKey);
+
+        assertEq(
+            state.getAddress(),
+            ownerAddr
+        );
+    }
+
+    function testAuthorizeAddrWithSignerAsOwner() public {
+        vm.prank(ownerAddr);
+        router.authorizeAddr(dispatcher, 0, 0, sessionAddr);
+
+        // owner signs the message authorizing the session
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerKey, keccak256(abi.encodePacked(
+            PREFIX_MESSAGE,
+            (AUTHEN_MESSAGE.length+20).toString(),
+            AUTHEN_MESSAGE,
+            sessionAddr
+        )));
+
+        // relay submits the auth request on behalf of owner
+        vm.prank(relayAddr);
+        router.authorizeAddr(dispatcher, 0, 0, sessionAddr, v,r,s);
+
+        // should now be able to use sessionKey to act as owner
+        dispatchSigned(sessionKey);
+
+        assertEq(
+            state.getAddress(),
+            ownerAddr
+        );
+    }
+
+    function testRevokeAddrWithSenderAsOwner() public {
+        vm.prank(ownerAddr);
+        router.authorizeAddr(dispatcher, 0, 0, sessionAddr);
+        dispatchSigned(sessionKey);
+        assertEq(
+            state.getAddress(),
+            ownerAddr
+        );
+
+        // sender trusted to destroy their session
+        vm.prank(ownerAddr);
+        router.revokeAddr(sessionAddr);
+
+        // session signed actions should now fail...
+        vm.expectRevert(SessionUnauthorized.selector);
+        dispatchSigned(sessionKey);
+    }
+
+    function testRevokeAddrWithSignerAsOwner() public {
+        vm.prank(ownerAddr);
+        router.authorizeAddr(dispatcher, 0, 0, sessionAddr);
+        dispatchSigned(sessionKey);
+        assertEq(
+            state.getAddress(),
+            ownerAddr
+        );
+
+        // owner signs the message destroying the session
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerKey, keccak256(abi.encodePacked(
+            PREFIX_MESSAGE,
+            (REVOKE_MESSAGE.length+20).toString(),
+            REVOKE_MESSAGE,
+            sessionAddr
+        )));
+        vm.prank(relayAddr);
+        router.revokeAddr(sessionAddr, v,r,s);
+
+        // session signed actions should now fail...
+        vm.expectRevert(SessionUnauthorized.selector);
+        dispatchSigned(sessionKey);
+    }
+
+    // dispatches a SET_SENDER action with msg.sender set to relayAddr
+    // the LogSenderRule sets the state to the action's owner so we
+    // can confirm what the action got processed as
+    function dispatchSigned(uint256 privateKey) internal {
+        bytes memory action = abi.encodeCall(TestActions.SET_SENDER, ());
+        bytes32 digest = keccak256(abi.encodePacked(
+            "\x19Ethereum Signed Message:\n32",
+            keccak256(action)
+        ));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
+        vm.prank(relayAddr);
+        router.dispatch(
+            action,
+            v,r,s
+        );
+    }
+
+}
