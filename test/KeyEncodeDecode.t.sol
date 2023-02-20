f7fefdcd23afd35e94a8f1c612d916c7f3c20dd6
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-02-20 17:15:56 +0000

    string key encoding and decoding
    - Strings are encoded as fixed 20 character strings
    - When strings are decoded the length is found be either finding the first 0 byte in the 20 byte key or if we reach the end of the byte array

---
+++ test/KeyEncodeDecode.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import {State, CompoundKeyEncoder, CompoundKeyDecoder} from "../src/State.sol";
+
+interface Kind {
+    function Item() external;
+}
+
+contract KeyEncodeDecodeTest is Test {
+    function testStringKey() public {
+        // Test strings under 20 chars
+        string memory testKey = "0123456789"; 
+        bytes24 encodedKey = CompoundKeyEncoder.STRING(Kind.Item.selector, testKey);
+        string memory decodedKey = CompoundKeyDecoder.STRING(encodedKey);
+
+        assertEq(
+            decodedKey,
+            testKey,
+            "expected decoded key to equal original key"
+        );
+
+        // Test strings that are 20 chars. 
+        testKey = "01234567890123456789";  
+        encodedKey = CompoundKeyEncoder.STRING(Kind.Item.selector, testKey);
+        decodedKey = CompoundKeyDecoder.STRING(encodedKey);
+
+        assertEq(
+            decodedKey,
+            testKey,
+            "expected decoded key to equal original key"
+        );
+
+        // Test strings that are over 20 chars (Expected to truncate the key) 
+        testKey = "012345678901234567890123456789";
+        encodedKey = CompoundKeyEncoder.STRING(Kind.Item.selector, testKey);
+        decodedKey = CompoundKeyDecoder.STRING(encodedKey);
+
+        string memory testKeyTruncated = "01234567890123456789"; 
+        assertEq(
+            decodedKey,
+            testKeyTruncated,
+            "expected decoded key to equal truncated 20 char key"
+        );
+    }
+}
