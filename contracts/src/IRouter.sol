44586db86c912a76db9c9591cb99741abb60358c
chrisfarms <chris@farmiloe.com>
2023-08-08 10:28:59 +0100

    fmt

--- contracts/src/IRouter.sol
+++ contracts/src/IRouter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Dispatcher } from "./IDispatcher.sol";
+import {Dispatcher} from "./IDispatcher.sol";
 
 // Routers accept "signed" bundles of Actions and forwards them to Dispatcher.dispatch
 // They might be a seperate contract or an extension of the Dispatcher
